3b52951e006aca87add0d443279505c2cfd24554
Gary Hughes <gary.hughes87@virginmedia.com>
2020-09-11 14:14:07 +0000

    Added jQuery cards source code, images, and added comments on script examples

---
+++ script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+ 
+
+ 	$("#stream1_btn").on("click", function() { // or use .click(function() {
+ 		$(".stream1").removeClass('highlight_stream');
+		$(".stream2").removeClass('highlight_stream');
+		$(".stream3").removeClass('highlight_stream');
+	  	$(".stream1").addClass('highlight_stream');
+	});
+	$("#stream2_btn").on("click", function() { // for mouse hover use .mouseenter / mouseleave(function() { to switch back and forth or use .hover(function() {
+		$(".stream1").removeClass('highlight_stream');
+		$(".stream2").removeClass('highlight_stream');
+		$(".stream3").removeClass('highlight_stream');
+	  	$(".stream2").addClass('highlight_stream');
+	});
+	$("#stream3_btn").on("click", function() {
+		$(".stream1").removeClass('highlight_stream');
+		$(".stream2").removeClass('highlight_stream');
+		$(".stream3").removeClass('highlight_stream');
+	  	$(".stream3").addClass('highlight_stream');
+	});
+
+
+}); 
